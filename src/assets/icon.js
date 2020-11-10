ed4eadc796f2e09e72d5b042a49cfdf1d3364864
Tanisha <tanisha031199@gmail.com>
2020-11-11 00:50:11 +0530

    styles for section

---
+++ src/assets/icon.js
@@ -0,0 +1,9 @@
+import React, { Children } from 'react'
+
+export const Icons = {
+    right_arrow:(
+        <svg width="40" height="40" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+             <path d="M32.02 22H8V26H32.02V32L40 24L32.02 16V22Z" fill="#4F80E2"/>
+        </svg>
+    )
+};
--- src/components/SectionPartition.js
+++ src/components/SectionPartition.js
@@ -1,11 +1,13 @@
 import React, { Children } from 'react'
 import styled from 'styled-components';
 import {tokens} from "../../config/themes"
+// import ArrowIcon from '../assets/icons/right-arrow.svg';
+import {Icons} from "../assets/icon"
 
 const SectionWrapper=styled.div`
     // width:100%;
     background-color:${tokens.colors.secondary[1]};
-    padding:${tokens.space[8]};
+    padding:${tokens.space[10]};
     // position:relative;
     h2{
         font-size:${tokens.fontSizes[6]};
@@ -16,11 +18,17 @@ const SectionWrapper=styled.div`
     //     color:${tokens.colors.primary[0]};
     //     font-weight:${tokens.fontWeights[1]};
     // }
+    span{
+        display:flex;
+        align-items:center;
+        justify-content:flex-end;
+    }
     a{
         // position:absolute;
         // right:0;
-        float:right;
+        // float:right;
         text-decoration:none;
+        font-size:${tokens.fontSizes[5]};
         color:${tokens.colors.tertiary[0]};
         font-weight:${tokens.fontWeights[1]};
     }
@@ -45,7 +53,11 @@ export default function SectionPartition({header,description,linkLabel,children}
                 <p>{description}</p>
                 {children}
             </SectionContent>
-            <a href="#">{linkLabel}</a>
+            <span>
+                <a href="#">{linkLabel}</a>
+                {/* <ArrowIcon/>             */}
+                {Icons['right_arrow']}
+            </span>
         </SectionWrapper>
     )
 }
