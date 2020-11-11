93816a2ed14f3b9023ce984fbbee5184c0203ddb
Tanisha <tanisha031199@gmail.com>
2020-11-12 01:14:36 +0530

    refactor index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,6 @@
 import React from "react";
-// import { graphql } from 'gatsby'
-
-
 import Layout from "../components/Layout";
-// import Image from "../components/image";
-import { useStaticQuery, graphql } from "gatsby";
+import {graphql } from "gatsby";
 import Hero from "../components/Hero"
 import SEO from "../components/SEO";
 import SectionPartition from "../components/SectionPartition";
@@ -12,36 +8,13 @@ import SectionPartition from "../components/SectionPartition";
 const IndexPage = ({
   data : { site, booksQuery, notesQuery}
 }) => {
-  // const posts = useStaticQuery(
-  //   graphql`
-  //     query {
-  //       allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
-  //         nodes {
-  //           frontmatter {
-  //             title
-  //             date
-  //             path
-  //           }
-  //           excerpt
-  //         }
-  //       }
-  //     }
-  //   `
-  // );
   return (
     <Layout>
       <SEO title="Blog" />
-      {/* {posts.allMdx.nodes.map(p => (
-        <Post
-          title={p.frontmatter.title}
-          path={p.frontmatter.path}
-          date={p.frontmatter.date}
-          description={p.excerpt}
-        />
-      ))} */}
       <Hero />
-      {/* <h1>Tanisha Sabherwal</h1> */}
       <div>
+        
+    {/* Digital Garden  */}
       <SectionPartition 
           header="Digital Garden"
           description="A Software Engineer who talks about development, 
@@ -52,17 +25,14 @@ const IndexPage = ({
               <div>
               {
                 notesQuery && notesQuery.edges.map(i=>(
-                  <>
-                  {/* <p>{i.node.frontmatter.cover.childImageSharp.fluid.src}</p> */}
                   <p>{i.node.frontmatter.title}</p>
-                  {/* <p>{i.node.id}</p> */}
-                  </>
                 ))
               }
               </div>
-              {/* <a href="#">Visit the Garden</a> */}
             </div>
-          </SectionPartition>
+        </SectionPartition>
+
+      {/* Book Shelf */}
         <SectionPartition 
           header="Book Shelf"
           description="A Software Engineer who talks about development, 
@@ -73,25 +43,12 @@ const IndexPage = ({
               <div>
               {
                 booksQuery && booksQuery.edges.map(i=>(
-                  <>
-                  {/* <p>{i.node.frontmatter.cover.childImageSharp.fluid.src}</p> */}
                   <img src={i.node.frontmatter.cover.childImageSharp.fluid.src}/>
-                  {/* <p>{i.node.id}</p> */}
-                  </>
                 ))
               }
               </div>
-              {/* <a href="#">Visit the Garden</a> */}
             </div>
-          </SectionPartition>
-        <h2>Garden</h2>
-        <div>
-            {
-              booksQuery && booksQuery.edges.map(i=>(
-                <p>{i.node.id}</p>
-              ))
-            }
-        </div>
+        </SectionPartition>
       </div>
     </Layout>
   );
