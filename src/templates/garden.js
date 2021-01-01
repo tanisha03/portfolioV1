8062095187dde082ca9ace4cd815d8e885f1c889
Tanisha <tanisha031199@gmail.com>
2021-01-02 01:05:57 +0530

    styled template for garden

--- src/templates/garden.js
+++ src/templates/garden.js
@@ -1,14 +1,17 @@
 import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
+import {Heading} from "./shared"
 
-const GardenPage = ({children}) => (
-  <Layout>
-    <SEO title="Garden" />
-    <div style={{padding:"4%"}}>
-      {children}
-    </div>
-  </Layout>
-)
+const GardenPage = ({children,pageContext}) => {
+  return(
+    <Layout>
+      <SEO title="Garden" />
+      <div style={{padding:"4% 10%"}}>
+        <Heading>{pageContext?.frontmatter?.title}</Heading>
+        {children}
+      </div>
+    </Layout>
+)};
 
 export default GardenPage
---
+++ src/templates/shared/index.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Heading = styled.h1`
+  padding:3% 0;
+  border-bottom:0.5px solid black;
+  margin-bottom:2%;
+`;
