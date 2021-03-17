f8f93e7596af4a317c12b7acfb5e37c96f1e777e
Tanisha <tanisha031199@gmail.com>
2021-03-17 22:50:53 +0530

    added share to twitter card

--- src/templates/shared/ShareCard.js
+++ src/templates/shared/ShareCard.js
@@ -1,10 +1,31 @@
 import React from "react"
+import {Icons} from "../../assets/icon"
+import styled from 'styled-components';
 
+
+const ShareCardWrapper = styled.div`
+  display:block;
+  width:fit-content;
+  margin:0 auto;
+  font-size:1.5em;
+  padding: 5% 0 0;
+  a{
+    display:flex;
+    align-items:center;
+    text-align:center;
+    svg{
+      margin-left:12px;
+    }
+  }
+`;
 
 const ShareCard = ({link}) => (
-  <center style={{width:"100%"}}>
-    <a href={link} target="_blank">Share on Twitter</a>
-  </center>
+  <ShareCardWrapper>
+    <a href={link} rel="noreferrer" target="_blank">
+      Share on Twitter
+      {Icons['share']}
+    </a>
+  </ShareCardWrapper>
 )
 
 export default ShareCard
