fa332ad9459e6e3049dc218608b345b51c5b66f7
Tanisha <tanisha031199@gmail.com>
2021-03-29 01:07:17 +0530

    added code drawings page

--- src/pages/drawings.js
+++ src/pages/drawings.js
@@ -2,7 +2,37 @@ import React from "react"
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 import {graphql } from "gatsby";
+import styled from 'styled-components';
+import {tokens} from "../../config/themes"
 
+const MainWrapper = styled.div`
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+`;
+
+const DrawingsWrapper = styled.div`
+  margin:4% 0;
+  display:flex;
+  justify-content:space-between;
+  background-color:${tokens.colors.secondary[1]};
+  width:60%;
+//   @media only screen and (max-width: 576px) {
+//     div{
+//      width:100%;
+//      img{
+//        width:100%;
+//      }
+//     }
+//  }
+`;
+
+const DescriptionSection = styled.div`
+  padding:8% 6%;
+  h2{
+    margin-bottom:${tokens.space[6]};
+  }
+`;
 
 const CodeDrawingsPage = ({
   data : { site, drawingsQuery}
@@ -12,16 +42,20 @@ const CodeDrawingsPage = ({
     <center>
       <h1>Code Drawings</h1>
     </center>
-    <div style={{padding:"4%", display:"flex"}}>
+    <MainWrapper>
       {
         drawingsQuery.edges.map(item=>(
-          <div>
+          <DrawingsWrapper>
+            <DescriptionSection>
+              <h2>{item.node.frontmatter.title}</h2>
+              <p>{item.node.frontmatter.desc}</p>
+              <a href={item.node.frontmatter.cover.childImageSharp.fluid.src}>View</a>
+            </DescriptionSection>
             <img src={item.node.frontmatter.cover.childImageSharp.fluid.src} alt={item.node.frontmatter.title}/>
-            <div>{item.node.frontmatter.title}</div>
-          </div>
+          </DrawingsWrapper>
         ))
       }
-    </div>
+    </MainWrapper>
   </Layout>
 )
 
@@ -42,6 +76,7 @@ export const pageQuery = graphql`
             title
             date
             slug
+            desc
             cover {
               childImageSharp {
                 fluid(maxWidth: 320, maxHeight: 460) {
